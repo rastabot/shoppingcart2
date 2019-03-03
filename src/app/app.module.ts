5159548c57a59db9cabcf8254eefd7e06aae3d20
rastabot <donanttonio79@gmail.com>
2019-03-03 16:16:23 +0200

    march3/19 , search, buttons and nav working

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { DetailsComponent } from './details/details.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { SearchfilterPipe } from './searchfilter.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    DetailsComponent,
+    ContactComponent,
+    AboutComponent,
+    SearchfilterPipe,
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,93 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../one-product-detail';
+import { ProductsDataService } from '../products-data.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  products : Product[];
+  total:number = 0;
+  numberOfItems:number ;
+
+
+  constructor(private productService : ProductsDataService) { }
+
+
+  getProducts():void {
+    this.productService.getProducts()
+    .subscribe(products => this.products = products)
+  }
+  ngOnInit() {
+    this.getProducts();
+    this.totalPrice();
+    this.totalQuantity ()
+  }
+// for pipe filter 
+filtername="";
+
+//===buttons functions
+  totalPrice(){
+    this.total = 0;
+    for(var i=0;i<this.products.length;i++){
+      this.total += (this.products[i].product_price * 
+        this.products[i].product_quantity);
+    }
+  }
+
+  totalQuantity (){
+    this.numberOfItems=0;
+    for(var i=0;i<this.products.length;i++){
+      this.numberOfItems += this.products[i].product_quantity
+    }
+
+  }
+
+
+  add(pid: Number){
+    console.log(pid);
+    for(var i=0;i<this.products.length;i++){
+      if(this.products[i].product_id === pid)
+      {  
+        this.products[i].product_quantity += 1;
+      }           
+    }
+    this.totalPrice();
+    this.totalQuantity();
+    console.log(this.products);
+  }
+
+  del(pid: Number){
+    console.log(pid);
+    for(var i=0;i<this.products.length;i++){
+      if(this.products[i].product_id === pid && this.products[i].product_quantity>0)
+      {  
+        this.products[i].product_quantity -= 1;
+        
+      }           
+    }
+    this.totalPrice();
+    this.totalQuantity();
+    console.log(this.products);
+  }
+
+  delpopup(pid: Number){
+    console.log(pid);
+    for(var i=0;i<this.products.length;i++){
+      if(this.products[i].product_id === pid)
+      {  
+        this.products.splice(i,1);
+      }           
+    }
+    this.totalPrice();
+    this.totalQuantity();
+    console.log(this.products);
+  }
+
+
+
+
+}
